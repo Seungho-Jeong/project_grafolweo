6134cc5a3c0fecbec439ed9a17744ffa71cffb81
Daniel Jeong <sh007.jeong@gmail.com>
2020-10-22 15:21:26 +0900

    Add: work(아트워크) App. 생성 및 모델 작성 - 아트워크(work) App. 신규생성 - 작품, 배경화면, 댓글, 태그 관련 클래스 작성 - ERD: https://aquerytool.com:443/aquerymain/index/?rurl=581ffee9-9665-4a2d-bd14-5caeeed378a8& - P/W: 61x206 Modity: User 클래스 및 Settings.py 수정 - User ~ Follow 자기참조를 위하여 Follow 클래스 생성 - Settings.py 내 work App 추가

--- user/models.py
+++ user/models.py
@@ -1,17 +1,24 @@
 from django.db      import models
-#from work.models    import Work
 
 class User(models.Model):
     user_name           = models.CharField(max_length = 40)
     email               = models.CharField(max_length = 50)
     mobile              = models.CharField(max_length = 40)
-    password            = models.CharField(max_length = 100)
+    password            = models.CharField(max_length = 200)
     introduction        = models.CharField(max_length = 300, null = True)
     profile_image_url   = models.CharField(max_length = 1000, null = True)
     created_at          = models.DateTimeField(auto_now_add = True)
     updated_at          = models.DateTimeField(auto_now = True)
+    following           = models.ManyToManyField(
+        "self", symmetrical = False, through = "Follow", related_name = "user_to_follow"
+    )
 
     class Meta:
-        db_table = "Users"
+        db_table = "users"
 
+class Follow(models.Model):
+    follower            = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "follower")
+    creator             = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "creator")
 
+    class Meta:
+        db_table = "follows"
