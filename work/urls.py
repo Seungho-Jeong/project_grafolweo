ca9ad3219d8054daa6bef21bfe4a6e162d8dfdb4
kimst5418 <kimst5418@gmail.com>
2020-10-25 13:42:10 +0900

    Add : 배경화면 메인페이지 인기 크리에이터와 팔로우 기능 엔드포인트 추가 - 인기 크리에이터 엔드포인트 구현 - 인기 크리에이터를 팔로우하고 취소 할 수 있는 엔드포인트 구현

--- work/urls.py
+++ work/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from work.views  import (
+    TopCreatorsView,
+    WallpaperMainFollowView,
+)
 
 urlpatterns = [
+    path('/wallpaper/topcreators', TopCreatorsView.as_view()),
+    path('/wallpaper/follow', WallpaperMainFollowView.as_view()),
 ]
--- work/views.py
+++ work/views.py
@@ -1,6 +1,7 @@
 import json
 from django.http  import JsonResponse 
 from django.views import View
+from django.db    import IntegrityError
 from user.models  import User, Follow
 from work.models  import (
     Category,
@@ -18,3 +19,45 @@ from work.models  import (
     Reply,
     ReplyLike
 )
+
+class TopCreatorsView(View) :
+    
+    def get(self, request) : 
+        user_id     = 10    # 임시 데이터, 추후 토큰 데코레이터를 통해 user_id 받을 예정
+        creators    = User.objects.all()
+        creatorlist = [ {
+            "id"                : creator.id,
+            "user_name"         : creator.user_name,
+            "profile_image_url" : creator.profile_image_url,
+            "followBtn"         : creator.creator.filter(follower_id=user_id).exists()
+        } for creator in creators ][:9]
+        return JsonResponse({'topCreators': creatorlist }, status=200)
+
+class WallpaperMainFollowView(View) :
+    
+    def post(self, request) : 
+        try :
+            data       = json.loads(request.body) 
+            user_id    = 10    # 임시 데이터, 추후 토큰 데코레이터를 통해 user_id 받을 예정
+            creator_id = data['creator_id']
+            follow     = Follow.objects.get(follower_id = user_id, creator_id = creator_id)
+            follow.delete()
+            message = {
+                "id"        : creator_id,
+                "followBtn" : False
+            }
+            return JsonResponse({'data': message }, status=200)
+        except Follow.DoesNotExist :
+            try :
+                Follow.objects.create( follower_id = user_id, creator_id = creator_id)
+                message = {
+                    "id"        : creator_id,
+                    "followBtn" : True
+                }
+                return JsonResponse({'data': message }, status=200)
+            except IntegrityError :
+                return JsonResponse({'Error': "unknown user" }, status=400)
+        except KeyError as e :
+            return JsonResponse({'Key Error': str(e) }, status=400)
+        except json.decoder.JSONDecodeError :
+            return JsonResponse({'Error': "json data error" }, status=400)
