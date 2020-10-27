0601268285a91402a069af17211dda2bad76fed6
Daniel Jeong <sh007.jeong@gmail.com>
2020-10-27 13:20:27 +0900

    Modify: 불필요 문장 삭제, Status code 변경 및 에러 처리방식 변경 등 - 배포 전 테스트 목적으로 작성한 코드 삭제 - 자원 생성 결과 메시지 Status code를 200에서 201로 변경 - 권한 관련 에러 메시지 Status code를 400에서 401로 변경 - Get method로 OBJ를 조회하는 경우 발생하는 에러 캐치를   if statement 방식에서  Model.DoesNotExist형으로 변경 - Request body 중 형식을 유지해야 하는 값(email, mobile)은 포맷을 식별토록 정규식 적용

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 import json
 import bcrypt
 import jwt
+import re
 import my_settings
 
 from django.http import JsonResponse, HttpResponse
@@ -10,94 +11,65 @@ from django.db.models import Q
 from .models import User
 
 
-class SignUpView(View):  # 클래스명 협의 필요
+class SignUpView(View):
     def post(self, request):
         data = json.loads(request.body)
-        print("제이슨 로즈 성공")
-        try:
-            if not (data["email"] and data["mobile"] and data["user_name"]):
-                return JsonResponse({"message": "NO_VALUES"}, status=400)
 
+        try:
             if User.objects.filter(email=data["email"]).exists():
-                return JsonResponse({"message": "EXISTS_EMAIL"}, status=400)
+                return JsonResponse({"MESSAGE": "EXISTS_EMAIL"}, status=400)
 
             if User.objects.filter(mobile=data["mobile"]).exists():
-                return JsonResponse({"message": "EXISTS_MOBILE"}, status=400)
+                return JsonResponse({"MESSAGE": "EXISTS_MOBILE"}, status=400)
 
             if len(data["password"]) < 8:
-                return JsonResponse({"message": "PW_IS_TOO_SHORT"}, status=400)
+                return JsonResponse({"MESSAGE": "PW_IS_TOO_SHORT"}, status=400)
 
-            if "@" not in str(data["email"]) or "." not in str(data["email"]):
-                return JsonResponse({"message": "NOT_IN_EMAIL_FORM"}, status=400)
+            if not re.match("^01(0|1|6|7|8|9)([0-9]{3,4})([0-9]{4})$", data["mobile"]):
+                return JsonResponse({"MESSAGE": "NOT_IN_PHONE_NUMBER_FROM"}, status=400)
+
+            if not re.match("^[a-zA-Z0-9]([-_\.]?[a-zA-Z0-9])*@[a-zA-Z0-9]([-_\.]?[a-zA-Z0-9])*\.[a-zA-Z]{2,}$", data["email"]):
+                return JsonResponse({"MESSAGE": "NOT_IN_EMAIL_FORM"}, status=400)
 
             else:
                 hashed_password = bcrypt.hashpw(
                     data["password"].encode("UTF-8"), bcrypt.gensalt())
                 User.objects.create(
-                    user_name=data["user_name"],
-                    email=data["email"],
-                    mobile=data["mobile"],
-                    password=hashed_password.decode("UTF-8")
+                    user_name         = data["user_name"],
+                    email             = data["email"],
+                    mobile            = data["mobile"],
+                    password          = hashed_password.decode("UTF-8"),
+                    introduction      = data["introduction"],
+                    profile_image_url = data["profile_image_url"]
                 )
-                return JsonResponse({"message": "SUCCESS"}, status=200)
+                return JsonResponse({"MESSAGE": "REGISTRATION_SUCCESS"}, status=201)
 
         except KeyError as e:
-            return JsonResponse({"message": f"KEY_ERROR:{e}"}, status=400)
-
-        except ValueError as e:
-            return JsonResponse({"message": f"VALUE_ERROR:{e}"}, status=400)
-
-        except:
-            return JsonResponse({"message": "INVALID_ERROR"}, status=400)
-
-    def get(self, request):
-        try:
-            user_data = User.objects.values()
-            return JsonResponse({"user": list(user_data)}, status=200)
-
-        except KeyError as e:
-            return JsonResponse({"message": f"KEY_ERROR:{e}"}, status=400)
-
-        except ValueError as e:
-            return JsonResponse({"message": f"VALUE_ERROR:{e}"}, status=400)
-
-        except:
-            return JsonResponse({"message": "INVALID_ERROR"}, status=400)
-
+            return JsonResponse({"MESSAGE": f"KEY_ERROR:{e}"}, status=400)
 
 class LoginView(View):
     def post(self, request):
 
         try:
-            data = json.loads(request.body)
+            data        = json.loads(request.body)
             given_email = data["email"]
-            given_pw = data["password"]
+            given_pw    = data["password"]
 
-            if User.objects.filter(email=given_email).exists():
-                account = User.objects.get(email=given_email)
+            account     = User.objects.get(email=given_email)
 
-                if account:
-
-                    if bcrypt.checkpw(given_pw.encode("UTF-8"), account.password.encode("UTF-8")):
-                        token = jwt.encode(
-                            {"email": given_email},
-                            my_settings.SECRET["secret"],
-                            algorithm=my_settings.ALGORITHM["algorithm"]
-                        )
-                        token = token.decode("UTF-8")
-                        return JsonResponse({"message": "SUCCESS", "token": token}, status=200)
-
-                    else:
-                        return JsonResponse({"message": "WRONG_PASSWORD"}, status=401)
+            if bcrypt.checkpw(given_pw.encode("UTF-8"), account.password.encode("UTF-8")):
+                token = jwt.encode(
+                    {"email": given_email},
+                    my_settings.SECRET["secret"],
+                    algorithm=my_settings.ALGORITHM["algorithm"]
+                )
+                return JsonResponse({"MESSAGE": "LOGIN_SUCCESS", "token": token.decode("UTF-8")}, status=200)
 
             else:
-                return JsonResponse({"message": "DOES_NOT_EXIST_USER"}, status=401)
+                return JsonResponse({"MESSAGE": "WRONG_PASSWORD"}, status=401)
+
+        except User.DoesNotExist:
+                return JsonResponse({"MESSAGE": "DOES_NOT_EXIST_USER"}, status=401)
 
         except KeyError as e:
-            return JsonResponse({"message": f"KEY_ERROR:{e}"}, status=400)
-
-        except ValueError as e:
-            return JsonResponse({"message": f"VALUE_ERROR:{e}"}, status=400)
-
-        except:
-            return JsonResponse({"message": "INVALID_ERROR"}, status=400)
+            return JsonResponse({"MESSAGE": f"KEY_ERROR:{e}"}, status=401)
