d5f736e3349816b6c6ef2b601cdac348c2676e61
Seungho Jeong <sh007.jeong@gmail.com>
2021-02-19 20:47:01 +0900

    부분 리팩토링 및 주석

--- user/views.py
+++ user/views.py
@@ -12,9 +12,24 @@ from .models          import User
 
 class SignUpView(View):
     def post(self, request):
-        data = json.loads(request.body)
+        """
+        유저 회원가입
+        :param request:
+            user_name,
+            email,
+            mobile,
+            password,
+            profile_image_url,
+            introduction
+        :return:
+            201: success
+            400: exception error
+            401: key error
+        """
 
         try:
+            data = json.loads(request.body)
+
             if User.objects.filter(email=data["email"]).exists():
                 return JsonResponse({"MESSAGE": "EXISTS_EMAIL"}, status=400)
 
@@ -33,44 +48,54 @@ class SignUpView(View):
             hashed_password = bcrypt.hashpw(
                 data["password"].encode("UTF-8"), bcrypt.gensalt())
             User.objects.create(
-                user_name=data["user_name"],
-                email=data["email"],
-                mobile=data["mobile"],
-                password=hashed_password.decode("UTF-8"),
-                introduction=data["introduction"],
-                profile_image_url=data["profile_image_url"]
+                user_name        = data["user_name"],
+                email            = data["email"],
+                mobile           = data["mobile"],
+                password         = hashed_password.decode("UTF-8"),
+                introduction     = data["introduction"],
+                profile_image_url= data["profile_image_url"]
             )
             return JsonResponse({"MESSAGE": "SUCCESS"}, status=201)
 
         except KeyError as e:
-            return JsonResponse({"MESSAGE": f"KEY_ERROR:{e}"}, status=400)
+            return JsonResponse({"MESSAGE": "KEY_ERROR {}".format(e)}, status=401)
         except Exception as e:
-            return JsonResponse({"MESSAGE": f"{e}_ERROR!"}, status=400)
+            return JsonResponse({"MESSAGE": "ERROR {}".format(e)}, status=400)
 
 class LoginView(View):
     def post(self, request):
+        """
+        유저 로그인
+        :param request:
+            email,
+            password
+        :return:
+            200: success
+            401: input error, key error
+            400: exception error
+        """
 
         try:
-            data = json.loads(request.body)
-            given_email = data["email"]
-            given_pw = data["password"]
-
-            user = User.objects.get(email=given_email)
+            data    = json.loads(request.body)
+            user    = User.objects.get(email=data["email"])
             user_id = user.id
 
-            if bcrypt.checkpw(given_pw.encode("UTF-8"), user.password.encode("UTF-8")):
-                token = jwt.encode(
+            if not bcrypt.checkpw(data["password"].encode("UTF-8"), user.password.encode("UTF-8")):
+                return JsonResponse({"MESSAGE": "INVALID_INPUT"}, status=401)
+
+            token = jwt.encode(
                     {"user_id": user_id},
                     my_settings.SECRET["secret"],
-                    algorithm=my_settings.ALGORITHM["algorithm"]
-                )
-                return JsonResponse({"MESSAGE": "LOGIN_SUCCESS", "Authorization": token.decode("UTF-8")}, status=200)
+                    algorithm = my_settings.ALGORITHM["algorithm"]
+            )
 
-            return JsonResponse({"MESSAGE": "INVALID_INPUT"}, status=401)
+            return JsonResponse({
+                        "MESSAGE"       : "LOGIN_SUCCESS",
+                        "Authorization" : token.decode("UTF-8")
+                        }, status=200)
 
         except User.DoesNotExist:
             return JsonResponse({"MESSAGE": "INVALID_INPUT"}, status=401)
-
         except KeyError as e:
             return JsonResponse({"MESSAGE": f"KEY_ERROR:{e}"}, status=401)
         except Exception as e:
